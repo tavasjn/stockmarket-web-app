4e6681172af7812113fd57fc727fb6bd764ad10a
Tavas  Neuwirth <tavasneuwirth@tavass-mbp.lan>
2020-04-04 12:37:53 -0600

    working on styling

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/', function (req, res) {
     });
 });
 
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
+
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
