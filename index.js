5cdc815e29dad6e72cd3d1b6f47b4ba59118d035
Tavas  Neuwirth <tavasneuwirth@tavass-mbp.lan>
2020-04-05 16:01:04 -0600

    need to work on notify system

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// API KEY pk_503f885a86d943f3928788e9ea432964
+
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
@@ -10,50 +12,51 @@ const PORT = process.env.PORT || 5000;
 // use body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// API KEY pk_503f885a86d943f3928788e9ea432964
-
+// create call_api function
 function call_api(finishedAPI, ticker) {
   request(
-    'https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_503f885a86d943f3928788e9ea432964',
+    "https://cloud.iexapis.com/stable/stock/" +
+      ticker +
+      "/quote?token=pk_503f885a86d943f3928788e9ea432964",
     { json: true },
     (err, res, body) => {
       if (err) {
         return console.log(err);
       }
       if (res.statusCode === 200) {
-        // console.log(body);
+        //console.log(body);
         finishedAPI(body);
       }
     }
   );
 }
 
-//Set handlebars MiddleWare
+// Set Handlebars Middleware
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-const otherstuff = "hello there this is other stuff";
 
-// Set Handlebar GET Index routes
+// Set handlebar index GET route
 app.get("/", function (req, res) {
   call_api(function (doneAPI) {
     res.render("home", {
       stock: doneAPI,
     });
-  });
+  }, "fb");
 });
 
-// Set Handlebar POST Index route
+// Set handlebar index POST route
 app.post("/", function (req, res) {
   call_api(function (doneAPI) {
-    // posted_stuff = req.body.stock_ticker;
+    //console.log(doneAPI);
+    //posted_stuff = req.body.stock_ticker;
     res.render("home", {
       stock: doneAPI,
-      // posted_stuff: posted_stuff
     });
   }, req.body.stock_ticker);
 });
 
+// create about page route
 app.get("/about.html", function (req, res) {
   res.render("about");
 });
@@ -61,4 +64,4 @@ app.get("/about.html", function (req, res) {
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => console.log("Server Listenting on port " + PORT));
+app.listen(PORT, () => console.log("Server Listening on port " + PORT));
