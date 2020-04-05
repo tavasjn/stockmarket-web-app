c1730833d9c83406a05afc7f3b023ef8937f9237
Tavas  Neuwirth <tavasneuwirth@tavass-mbp.lan>
2020-04-04 20:15:03 -0600

    working on what stocks to add

--- index.js
+++ index.js
@@ -2,24 +2,45 @@ const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
 const path = require("path");
+const request = require("request");
 
 const PORT = process.env.PORT || 5000;
+
+// API KEY pk_503f885a86d943f3928788e9ea432964
+
+function call_api(finishedAPI) {
+  request(
+    "https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_503f885a86d943f3928788e9ea432964",
+    { json: true },
+    (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      if (res.statusCode === 200) {
+        // console.log(body);
+        finishedAPI(body);
+      };
+    }
+  );
+};
 
 //Set handlebars MiddleWare
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-const otherstuff = 'hello there this is other stuff';
+const otherstuff = "hello there this is other stuff";
 
 // Set Handlebar routes
 app.get('/', function (req, res) {
-    res.render('home', {
-        stuff: otherstuff
+    call_api(function(doneAPI) {
+        res.render('home', {
+            stock: doneAPI
+        });
     });
 });
 
-app.get('/about.html', function (req, res) {
-    res.render('about');
+app.get("/about.html", function(req, res) {
+  res.render("about");
 });
 
 // Set static folder
